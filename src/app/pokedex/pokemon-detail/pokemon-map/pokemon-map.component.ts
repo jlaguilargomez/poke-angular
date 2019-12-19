108eed0d95dff966fb7d85f7e9a8d72cc141b77f
José Ángel Cívico Martos <jacivico@indra.es>
2019-12-19 16:14:19 +0100

    Add Project Setup

---
+++ src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-map',
+  templateUrl: './pokemon-map.component.html',
+  styleUrls: ['./pokemon-map.component.scss']
+})
+export class PokemonMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
