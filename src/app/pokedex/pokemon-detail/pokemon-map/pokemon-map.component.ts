75c6d3c85af525aa371e3fb6db20039aa596d9fb
Gabriel García Rodríguez <ggarciaro@minsait.com>
2019-12-20 14:30:04 +0100

    Map is added and render the pokemons list using an observer. TODO: center the map dependening the selected pokemon usin the route

--- src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
+++ src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-pokemon-map',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonMapComponent implements OnInit {
 
+  @Input() pokemonObservable;
+
+
   constructor() { }
 
   ngOnInit() {
+    // Leaflet setting
+    let options = {attributionControl: false};
+    let map = L.map('map', options).setView([40.416775, -3.703790], 13);
+    let tileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
+    tileLayer.addTo(map)
+
+    this.pokemonObservable.subscribe(pokemons =>{
+      pokemons.forEach( pokemon => {
+        let marker = L.marker([pokemon.coord.lat, pokemon.coord.long],{icon: L.icon({iconUrl: pokemon.imagePath, iconSize :70})})
+        marker.addTo(map)
+      })
+    })
   }
 
 }
