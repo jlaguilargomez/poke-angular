cf735ad8c5d2c7f7ab8a0dc5f2b201d189cc4e0e
Gabriel García Rodríguez <ggarciaro@minsait.com>
2019-12-23 11:55:41 +0100

    Fly functionality is tested in the map with a non definituve button (TODO: Fly will change the focus with the selected pokemon in the final version)

--- src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
+++ src/app/pokedex/pokemon-detail/pokemon-map/pokemon-map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import * as L from 'leaflet';
+import { Pokemon } from '../../../interface/pokemon.interface';
 
 @Component({
   selector: 'app-pokemon-map',
@@ -9,23 +10,45 @@ import * as L from 'leaflet';
 export class PokemonMapComponent implements OnInit {
   @Input() pokemonObservable;
 
+  map: any;
+  pokemons: Pokemon[];
+  selectedPokemon: number = 0;
+
+  changePokemon() {
+    this.selectedPokemon++;
+    this.map.flyTo(
+      [
+        this.pokemons[this.selectedPokemon].coord.lat,
+        this.pokemons[this.selectedPokemon].coord.long,
+      ],
+      13
+    );
+  }
+
   constructor() {}
 
   ngOnInit() {
-    // Leaflet setting
-    let options = { attributionControl: false };
-    let map = L.map('map', options).setView([40.416775, -3.70379], 13);
-    let tileLayer = L.tileLayer(
-      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-      {
-        attribution:
-          'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-      }
-    );
-    tileLayer.addTo(map);
-
     this.pokemonObservable.subscribe(pokemons => {
-      console.log(pokemons);
+      this.pokemons = pokemons;
+
+      // Leaflet setting
+      let options = { attributionControl: false };
+      let map = L.map('map', options).setView(
+        [
+          pokemons[this.selectedPokemon].coord.lat,
+          pokemons[this.selectedPokemon].coord.long,
+        ],
+        13
+      );
+      let tileLayer = L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        {
+          attribution:
+            'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        }
+      );
+      tileLayer.addTo(map);
+
       pokemons.forEach(pokemon => {
         let pokemonIcon = L.icon({
           iconUrl: pokemon.imagePath,
@@ -45,6 +68,8 @@ export class PokemonMapComponent implements OnInit {
         });
         marker.addTo(map);
       });
+
+      this.map = map;
     });
   }
 }
